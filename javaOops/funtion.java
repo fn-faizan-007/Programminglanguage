a0db9799ac5045517024419f5865c77221e21de6
Muhammad Faizan <faizanweb187@gmail.com>
2026-09-01 10:16:41 +0500

    add all language file:

---
+++ javaOops/funtion.java
@@ -0,0 +1,17 @@
+package javaOops;
+
+
+ class funtion{
+    public static void main(String[] args) {
+         int[] makes={12,34,35,67,8};
+          for(int i=0;i<=5;i++){
+            if (makes[i]==8){
+                System.out.println(makes[i]);
+
+            }
+        }
+
+    }
+ }
+
+
---
+++ javaOops/output.java
@@ -0,0 +1,13 @@
+package java;
+
+/**
+ * output
+ */
+public class output {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+    }
+
+    
+}
