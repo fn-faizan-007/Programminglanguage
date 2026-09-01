458639fb9cfd3f4239e0113ccd91d327c9083167
Muhammad Faizan <faizanweb187@gmail.com>
2026-09-01 10:31:37 +0500

    correct java package problem and html file allpy style for betterment

--- javaOops/funtion.java
+++ javaOops/funtion.java
@@ -1,12 +1,15 @@
 package javaOops;
-
+import java.util.*;
 
  class funtion{
     public static void main(String[] args) {
-         int[] makes={12,34,35,67,8};
-          for(int i=0;i<=5;i++){
-            if (makes[i]==8){
-                System.out.println(makes[i]);
+        Scanner sc= new Scanner(System.in);
+        int[] makes={12,34,35,67,8};
+        System.out.print("Enter a num that you want to find out:  ");
+         int find=sc.nextInt();
+          for(int i=0;i<=makes.length;i++){
+            if (makes[i]==find){
+                System.out.println(makes[i]+" And this num will be in the index of: "+i);
 
             }
         }
--- javaOops/output.java
+++ javaOops/output.java
@@ -1,4 +1,4 @@
-package java;
+package javaOops;
 
 /**
  * output
