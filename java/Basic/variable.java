55134c6b904fa760710f017b0dad4cd5dfa0681d
Muhammad Faizan <faizanweb187@gmail.com>
2026-09-16 19:07:59 +0500

    done all file of 0 weak:

---
+++ java/Basic/variable.java
@@ -0,0 +1,25 @@
+package Basic;
+public class variable {
+    public static void main(String[] args) {
+
+        // Variables
+        int age = 18;
+        String name = "Faizan";
+        double marks = 85.5;
+        boolean passed = true;
+
+        // Concatenation
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Passed: " + passed);
+
+        // Change value
+        age = 20;
+        System.out.println("New Age: " + age);
+
+        // Commented-out code
+        // System.out.println(age.getClass());
+        // typeof(age);
+    }
+}
